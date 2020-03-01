2e464b5eabdb851c0de2442c6942727258c8f2ad
Max Bradley <maxbradley64@gmail.com>
2020-03-01 13:31:34 +0000

    Fixed issue occuring when subtracting from negative numbers

--- WebApiCalculator/Resources/RegExPatterns.cs
+++ WebApiCalculator/Resources/RegExPatterns.cs
@@ -14,6 +14,6 @@ namespace WebApiCalculator.Resources
 
         public static Regex AdditionCalculationPattern = new Regex("^.*?((\\d+\\.?\\d*)(\\+)(\\d+\\.?\\d*)).*?$");
 
-        public static Regex SubtractionCalculationPattern = new Regex("^.*?((\\d+\\.?\\d*)(\\-)(\\d+\\.?\\d*)).*?$");
+        public static Regex SubtractionCalculationPattern = new Regex("^.*?((\\-?\\d+\\.?\\d*)(\\-)(\\d+\\.?\\d*)).*?$");
     }
 }

2d4c08545764f7cb89e1a6a383988806d82d6e80
Max Bradley <maxbradley64@gmail.com>
2020-03-01 12:43:31 +0000

    Added unit test for calculations yielding a decimal result

--- UnitTests/CalculationTests.cs
+++ UnitTests/CalculationTests.cs
@@ -15,7 +15,7 @@ namespace UnitTests
         {
             var calculationResult = CalculationHelper.GetExpressionResult("1+3");
 
-            Assert.AreEqual(calculationResult, "4");
+            Assert.AreEqual("4", calculationResult);
         }
 
         [Test]
@@ -23,7 +23,7 @@ namespace UnitTests
         {
             var calculationResult = CalculationHelper.GetExpressionResult("44-3");
 
-            Assert.AreEqual(calculationResult, "41");
+            Assert.AreEqual("41", calculationResult);
         }
 
         [Test]
@@ -31,7 +31,7 @@ namespace UnitTests
         {
             var calculationResult = CalculationHelper.GetExpressionResult("12*4");
 
-            Assert.AreEqual(calculationResult, "48");
+            Assert.AreEqual("48", calculationResult);
         }
 
         [Test]
@@ -39,7 +39,7 @@ namespace UnitTests
         {
             var calculationResult = CalculationHelper.GetExpressionResult("12/4");
 
-            Assert.AreEqual(calculationResult, "3");
+            Assert.AreEqual("3", calculationResult);
         }
 
         [Test]
@@ -47,7 +47,7 @@ namespace UnitTests
         {
             var calculationResult = CalculationHelper.GetExpressionResult("1+3+9");
 
-            Assert.AreEqual(calculationResult, "13");
+            Assert.AreEqual("13", calculationResult);
         }
 
         [Test]
@@ -55,7 +55,7 @@ namespace UnitTests
         {
             var calculationResult = CalculationHelper.GetExpressionResult("44-3-12");
 
-            Assert.AreEqual(calculationResult, "29");
+            Assert.AreEqual("29", calculationResult);
         }
 
         [Test]
@@ -63,7 +63,7 @@ namespace UnitTests
         {
             var calculationResult = CalculationHelper.GetExpressionResult("12*4*2");
 
-            Assert.AreEqual(calculationResult, "96");
+            Assert.AreEqual("96", calculationResult);
         }
 
         [Test]
@@ -71,7 +71,7 @@ namespace UnitTests
         {
             var calculationResult = CalculationHelper.GetExpressionResult("12/4/3");
 
-            Assert.AreEqual(calculationResult, "1");
+            Assert.AreEqual("1", calculationResult);
         }
 
         [Test]
@@ -79,7 +79,7 @@ namespace UnitTests
         {
             var calculationResult = CalculationHelper.GetExpressionResult("12-25");
 
-            Assert.AreEqual(calculationResult, "-13");
+            Assert.AreEqual("-13", calculationResult);
         }
 
         [Test]
@@ -87,7 +87,7 @@ namespace UnitTests
         {
             var calculationResult = CalculationHelper.GetExpressionResult("12+39*2/0");
 
-            Assert.AreEqual(calculationResult, "Invalid expression - there was an attempt to divide by zero");
+            Assert.AreEqual("Invalid expression - there was an attempt to divide by zero", calculationResult);
         }
 
         [Test]
@@ -95,7 +95,15 @@ namespace UnitTests
         {
             var calculationResult = CalculationHelper.GetExpressionResult("1+10*4*2-8+0");
 
-            Assert.AreEqual(calculationResult, "73");
+            Assert.AreEqual("73", calculationResult);
+        }
+
+        [Test]
+        public void GivenCalculationResultingInDecimalCorrectResultIsReturned()
+        {
+            var calculationResult = CalculationHelper.GetExpressionResult("10/4");
+
+            Assert.AreEqual("2.5", calculationResult);
         }
     }
 }

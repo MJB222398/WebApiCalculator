e529fef8eb513f53491e6d04ba630562dfa8fc26
Max Bradley <maxbradley64@gmail.com>
2020-03-01 11:52:41 +0000

    Added calculation unit test for expressions with multiple instances of the same operator

--- UnitTests/CalculationTests.cs
+++ UnitTests/CalculationTests.cs
@@ -41,5 +41,37 @@ namespace UnitTests
 
             Assert.AreEqual(calculationResult, "3");
         }
+
+        [Test]
+        public void GivenMultipleAdditionCalculationCorrectResultIsReturned()
+        {
+            var calculationResult = CalculationHelper.GetExpressionResult("1+3+9");
+
+            Assert.AreEqual(calculationResult, "13");
+        }
+
+        [Test]
+        public void GivenMultipleSubtractionCalculationCorrectResultIsReturned()
+        {
+            var calculationResult = CalculationHelper.GetExpressionResult("44-3-12");
+
+            Assert.AreEqual(calculationResult, "29");
+        }
+
+        [Test]
+        public void GivenMultipleMultiplicationCalculationCorrectResultIsReturned()
+        {
+            var calculationResult = CalculationHelper.GetExpressionResult("12*4*2");
+
+            Assert.AreEqual(calculationResult, "96");
+        }
+
+        [Test]
+        public void GivenMultipleDivisionCalculationCorrectResultIsReturned()
+        {
+            var calculationResult = CalculationHelper.GetExpressionResult("12/4/3");
+
+            Assert.AreEqual(calculationResult, "1");
+        }
     }
 }
